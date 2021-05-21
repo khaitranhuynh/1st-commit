92e03f7e4462731ad6d06a15eda094b6bbd4980f
khaitranhuynh <38784446+khaitranhuynh@users.noreply.github.com>
2021-05-21 23:55:51 +0700

    tracuu va them phieunhap,phieuxuat
    tracuu va them phieunhap,phieuxuat

---
+++ QlKho/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QlKho.Models;
+
+namespace QlKho.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult TaoPhieuNhap()
+        {
+            return View();
+        }
+        public IActionResult TaoPhieuXuat()
+        {
+            return View();
+        }
+        public IActionResult ChuyenKho()
+        {
+            return View();
+        }
+        public IActionResult TraCuuPhieuNhap()
+        {
+            return View();
+        }
+        public IActionResult TraCuuPhieuXuat()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
